a0fb1d7611ab9422b8d72c39985cfee416a1be35
2Diomedes <t.martinteyssier@gmail.com>
2018-06-28 14:03:18 +0200

    Break

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
-  $('.add').click(function () {
+  $('.add').click(function() {
     var newT = $('.newT').val();
-    $("#columns").append("<li class='column' draggable='true'>"+newT+"</li>");
+    $("#columns").append("<li class='column' draggable='true'>" + newT + "</li>");
     $('.newT').val('');
 
     var dragSrcEl = null;
@@ -84,100 +84,109 @@ $(document).ready(function() {
 
   });
 
-  // var firstChild = $('.columns li :first-child');
-  //
-  // $('#work').click(function() {
-  //   $('.doing').append(firstChild);
-  //
-  // });
+  var firstChild = "";
+  var vf = false;
 
-  var pomodoro = {
-    started: false,
-    minutes: 0,
-    seconds: 0,
-    fillerHeight: 0,
-    fillerIncrement: 0,
-    interval: null,
-    minutesDom: null,
-    secondsDom: null,
-    fillerDom: null,
-    init: function() {
-      var self = this;
-      this.minutesDom = document.querySelector('#minutes');
-      this.secondsDom = document.querySelector('#seconds');
-      this.fillerDom = document.querySelector('#filler');
-      this.interval = setInterval(function() {
-        self.intervalCallback.apply(self);
-      }, 1000);
-      document.querySelector('#work').onclick = function() {
-        self.startWork.apply(self);
-      };
-      document.querySelector('#shortBreak').onclick = function() {
-        self.startShortBreak.apply(self);
-      };
-      document.querySelector('#longBreak').onclick = function() {
-        self.startLongBreak.apply(self);
-      };
-      document.querySelector('#stop').onclick = function() {
-        self.stopTimer.apply(self);
-      };
-    },
-    resetVariables: function(mins, secs, started) {
-      this.minutes = mins;
-      this.seconds = secs;
-      this.started = started;
-      this.fillerIncrement = 200 / (this.minutes * 60);
-      this.fillerHeight = 0;
-    },
-    startWork: function() {
-      this.resetVariables(25, 0, true);
-    },
-    startShortBreak: function() {
-      this.resetVariables(5, 0, true);
-    },
-    startLongBreak: function() {
-      this.resetVariables(15, 0, true);
-    },
-    stopTimer: function() {
-      this.resetVariables(25, 0, false);
-      this.updateDom();
-    },
-    toDoubleDigit: function(num) {
-      if (num < 10) {
-        return "0" + parseInt(num, 10);
-      }
-      return num;
-    },
-    updateDom: function() {
-      this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
-      this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
-      this.fillerHeight = this.fillerHeight + this.fillerIncrement;
-      this.fillerDom.style.height = this.fillerHeight + 'px';
-    },
-    intervalCallback: function() {
-      if (!this.started) return false;
-      if (this.seconds == 0) {
-        if (this.minutes == 0) {
-          this.timerComplete();
-          return;
-        }
-        this.seconds = 59;
-        this.minutes--;
-      } else {
-        this.seconds--;
-      }
-      this.updateDom();
-    },
-    timerComplete: function() {
-      this.started = false;
-      this.fillerHeight = 0;
+  $('#work').click(function() {
+
+    if (vf == false) {
+      firstChild = $('#columns li:first-child');
+      $('.doing').append(firstChild);
+      vf = true;
     }
-  };
-  window.onload = function() {
-    pomodoro.init();
-  };
+  });
+
+  $('#stop').click(function() {
+    $('.done').append(firstChild);
+  });
 
 
 
 
 });
+
+var pomodoro = {
+  started: false,
+  minutes: 0,
+  seconds: 0,
+  fillerHeight: 0,
+  fillerIncrement: 0,
+  interval: null,
+  minutesDom: null,
+  secondsDom: null,
+  fillerDom: null,
+  init: function() {
+    var self = this;
+    this.minutesDom = document.querySelector('#minutes');
+    this.secondsDom = document.querySelector('#seconds');
+    this.fillerDom = document.querySelector('#filler');
+    this.interval = setInterval(function() {
+      self.intervalCallback.apply(self);
+    }, 1000);
+    document.querySelector('#work').onclick = function() {
+      self.startWork.apply(self);
+    };
+    document.querySelector('#shortBreak').onclick = function() {
+      self.startShortBreak.apply(self);
+    };
+    document.querySelector('#longBreak').onclick = function() {
+      self.startLongBreak.apply(self);
+    };
+    document.querySelector('#stop').onclick = function() {
+      self.stopTimer.apply(self);
+    };
+  },
+  resetVariables: function(mins, secs, started) {
+    this.minutes = mins;
+    this.seconds = secs;
+    this.started = started;
+    this.fillerIncrement = 200 / (this.minutes * 60);
+    this.fillerHeight = 0;
+  },
+  startWork: function() {
+    this.resetVariables(0, 2, true);
+  },
+  startShortBreak: function() {
+    this.resetVariables(5, 0, true);
+  },
+  startLongBreak: function() {
+    this.resetVariables(15, 0, true);
+  },
+  stopTimer: function() {
+    this.resetVariables(25, 0, false);
+    this.updateDom();
+  },
+  toDoubleDigit: function(num) {
+    if (num < 10) {
+      return "0" + parseInt(num, 10);
+    }
+    return num;
+  },
+  updateDom: function() {
+    this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
+    this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
+    this.fillerHeight = this.fillerHeight + this.fillerIncrement;
+    this.fillerDom.style.height = this.fillerHeight + 'px';
+  },
+  intervalCallback: function() {
+    if (!this.started) return false;
+    if (this.seconds == 0) {
+      if (this.minutes == 0) {
+        this.timerComplete();
+        return;
+      }
+      this.seconds = 59;
+      this.minutes--;
+    } else {
+      this.seconds--;
+    }
+    this.updateDom();
+  },
+  timerComplete: function() {
+    this.started = false;
+    this.fillerHeight = 0;
+  }
+};
+window.onload = function() {
+  pomodoro.init();
+};
